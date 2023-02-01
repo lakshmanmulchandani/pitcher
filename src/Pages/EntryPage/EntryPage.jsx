f46c4284fbfa111ac8db0bb38ff60cac2b4509cd
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2023-02-02 00:21:34 +0530

    shifted buttons to app.js from entrypage

--- src/Pages/EntryPage/EntryPage.jsx
+++ src/Pages/EntryPage/EntryPage.jsx
@@ -6,9 +6,6 @@ function EntryPage() {
   return (
     <div>
       <h3>EntryPage</h3>
-      <h4 onClick={() => navigate("Portfolios")}>Portfolio</h4>
-      <h4 onClick={() => navigate("Buy")}>Buy</h4>
-      <h4 onClick={() => navigate("Admin")}>Admin</h4>
     </div>
   );
   return <div>EntryPage</div>;
--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import "./App.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
     <Router>
+      <App />
       <Allroutes />
     </Router>
   </React.StrictMode>
