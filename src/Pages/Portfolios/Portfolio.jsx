1f87263175f4609c328542d7425d79269fcf8739
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2023-02-01 13:16:05 +0530

    portfolio cards have been added,will edit more later as per requirements

--- src/Pages/Portfolios/Portfolio.jsx
+++ src/Pages/Portfolios/Portfolio.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
-function Portfolio() {
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core/";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import useStyles from "./styles";
+function Portfolio({post}) {
+  const classes = useStyles();
   return (
-    <div>Portfolio</div>
-  )
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={
+          // post.selectedFile ||
+          "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+        }
+        title={post.title}
+      />
+      <div className={classes.details}>
+        <Typography variant='body2' color='textSecondary' component='h2'>
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography
+        className={classes.title}
+        gutterBottom
+        variant='h5'
+        component='h2'>
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography variant='body2' color='textSecondary' component='p'>
+          {post.message}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
--- src/Pages/Portfolios/Portfolios.jsx
+++ src/Pages/Portfolios/Portfolios.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import Portfolio from "./Portfolio";
+import Grid from "@material-ui/core/Grid";
+import useStyles from "./styles";
+import "./Portfolio.css";
+const post = {
+  title: "Company Name",
+  message: "about company",
+  tags: ["tag1", "tag2", "tag3"],
+};
 function Portfolios() {
-  return <div>Portfolios</div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>portfolios</h1>
+      <div className='portfolio-container'>
+        <Portfolio post={post} />
+        <Portfolio post={post} />
+        <Portfolio post={post} />
+
+        <Portfolio post={post} />
+
+        <Portfolio post={post} />
+        <Portfolio post={post} />
+      </div>
+    </div>
+  );
 }
 
 export default Portfolios;
