3a70b84c66c37a74c4222d78c2d4001eda31dbc9
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2023-02-02 00:15:27 +0530

    changes router from app to index

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <h1> Welcome </h1>
-        <Allroutes />
-      </Router>
+      <h1>Welcome</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Allroutes from "./Allroutes";
+import {BrowserRouter as Router} from "react-router-dom";
+import "./App.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Router>
+      <Allroutes />
+    </Router>
   </React.StrictMode>
 );
 
