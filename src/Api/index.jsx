04c6b9b840ec0a24df63ff0f2e5b7374af8d2409
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2023-02-05 23:03:02 +0530

    render api added

--- src/Api/index.jsx
+++ src/Api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = "https://mymemories0809.herokuapp.com/posts";
-const url = "http://localhost:5000/Portfolios";
+// const url = "http://localhost:5000/Portfolios";
+const url = "https://pitcherfork.onrender.com/portfolios";
 
 export const fetchPortfolios = () => axios.get(url);
 export const createPortfolio = (newPortfolio) => axios.post(url, newPortfolio);
