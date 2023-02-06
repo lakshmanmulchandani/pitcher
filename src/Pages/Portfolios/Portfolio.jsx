8c679db1687d026b26a4b31afa037c0e598ba002
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2023-02-06 11:58:48 +0530

    data geting fetched in portfolio and mapped to buy page

--- src/Pages/Portfolios/Portfolio.jsx
+++ src/Pages/Portfolios/Portfolio.jsx
@@ -12,8 +12,10 @@ import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import useStyles from "./styles";
+
 function Portfolio({post}) {
   const classes = useStyles();
+
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -22,7 +24,7 @@ function Portfolio({post}) {
           // post.selectedFile ||
           "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
         }
-        title={post.title}
+        title={post.name}
       />
       <div className={classes.details}>
         <Typography variant='body2' color='textSecondary' component='h2'>
@@ -34,11 +36,11 @@ function Portfolio({post}) {
         gutterBottom
         variant='h5'
         component='h2'>
-        {post.title}
+        {post.name}
       </Typography>
       <CardContent>
         <Typography variant='body2' color='textSecondary' component='p'>
-          {post.message}
+          {post.about}
         </Typography>
       </CardContent>
     </Card>
