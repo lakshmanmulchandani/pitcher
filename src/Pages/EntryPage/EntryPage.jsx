684509012d8c4e6c197418ccc495c0229fcccd76
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2023-02-07 10:40:49 +0530

    Entry page frontend added

--- src/Pages/EntryPage/EntryPage.jsx
+++ src/Pages/EntryPage/EntryPage.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
+import "./EntryPage.css"
+import { Link, useNavigate } from "react-router-dom";
+
 
 function EntryPage() {
   const navigate = useNavigate();
   return (
-    <div>
-      <h3>EntryPage</h3>
+    <div className="entry">
+          <div class="container">
+        <div class="card">
+            <div class="content">
+                <h2>01</h2>
+                <h3>Your Code</h3>
+                
+                <p><input type="text" placeholder="Name" /></p>
+                <p><input type="text" placeholder="Code" /></p>
+                <Link to="#">submit</Link>
+            </div>
+        </div>
+    </div>
+    
     </div>
   );
-  return <div>EntryPage</div>;
 }
 
 export default EntryPage;
