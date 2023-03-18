075cb2f100dfe1f6f930383d3ce9cb23d372c75a
jayprakash02 <unijay12@gmail.com>
2023-03-19 05:14:59 +0530

    backend url updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const BACKEND_URL =
-  "https://u7mjakx4okt5junbeynyymxkcm0vdbne.lambda-url.ap-south-1.on.aws";
+  "https://13-233-198-227.nip.io:3443";
 // "http://localhost:5000";
 // "https://pitcherfork.onrender.com";
 // 'http://43.205.118.222:3000';
