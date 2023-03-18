ff09e3ac0944c18f9d7caee0bb3c8646d97f5fc4
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2023-03-18 19:18:43 +0530

    aws frontend url added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const BACKEND_URL =
-  // "http://localhost:5000";
-  "https://pitcherfork.onrender.com";
+  "https://u7mjakx4okt5junbeynyymxkcm0vdbne.lambda-url.ap-south-1.on.aws";
+// "http://localhost:5000";
+// "https://pitcherfork.onrender.com";
 // 'http://43.205.118.222:3000';
 export default BACKEND_URL;
